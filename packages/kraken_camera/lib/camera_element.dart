ec3a56b9b706347ddad8b48c6b791866619672d2
Tim <ReverseScale@iCloud.com>
2021-04-19 16:56:33 +0800

    feat: remove project info

--- packages/kraken_camera/lib/camera_element.dart
+++ packages/kraken_camera/lib/camera_element.dart
@@ -18,6 +18,7 @@ import 'camera.dart';
 const String CAMERA_PREVIEW = 'CAMERA-PREVIEW';
 
 final Map<String, dynamic> _defaultStyle = {
+  DISPLAY: BLOCK,
   WIDTH: ELEMENT_DEFAULT_WIDTH,
   HEIGHT: ELEMENT_DEFAULT_HEIGHT,
 };
