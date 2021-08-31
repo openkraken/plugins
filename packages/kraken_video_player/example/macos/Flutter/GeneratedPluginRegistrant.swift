c7b9af1e66fa4232dbfdd3904a43ea3282e9a21e
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-08-31 20:26:55 +0800

    release: 0.4.4

--- packages/kraken_video_player/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/kraken_video_player/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,13 +8,11 @@ import Foundation
 import connectivity_macos
 import kraken
 import kraken_video_player
-import path_provider_macos
 import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   KrakenPlugin.register(with: registry.registrar(forPlugin: "KrakenPlugin"))
   FLTKrakenVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FLTKrakenVideoPlayerPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
