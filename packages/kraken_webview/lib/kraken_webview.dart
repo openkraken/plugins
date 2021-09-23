b3c9917b5c28cb92609321b3633bdf5595b0c651
andycall <chenghuai.dtc@alibaba-inc.com>
2021-09-23 15:39:38 +0800

    feat: upgrade webview to quickjs

--- packages/kraken_webview/lib/kraken_webview.dart
+++ packages/kraken_webview/lib/kraken_webview.dart
@@ -3,27 +3,13 @@
  * Author: Kraken Team.
  */
 
-import 'dart:ffi';
 import 'package:kraken/bridge.dart';
 
-import 'platform.dart';
 import 'package:kraken/dom.dart';
 import 'iframe_element.dart';
 
-typedef Native_InitBridge = Void Function();
-typedef Dart_InitBridge = void Function();
-
-final Dart_InitBridge _initBridge = nativeDynamicLibrary
-    .lookup<NativeFunction<Native_InitBridge>>('initBridge')
-    .asFunction();
-
-void initBridge() {
-  _initBridge();
-}
-
 class KrakenWebView {
   static void initialize() {
-    initBridge();
     ElementManager.defineElement('IFRAME', (id, nativePtr, elementManager) {
       return IFrameElement(
           id, nativePtr.cast<NativeEventTarget>(), elementManager);
--- packages/kraken_webview/macos/kraken_webview.podspec
+++ packages/kraken_webview/macos/kraken_webview.podspec
@@ -15,7 +15,6 @@ A new flutter plugin project.
   s.source           = { :path => '.' }
   s.source_files     = 'Classes/**/*'
   s.dependency 'FlutterMacOS'
-  s.vendored_libraries = 'libkraken_webview.dylib'
   s.platform = :osx, '10.11'
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
   s.swift_version = '5.0'
