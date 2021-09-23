19b48a236a0c58af8568e3e9cb6da4a65a30ac51
andycall <chenghuai.dtc@alibaba-inc.com>
2021-09-23 14:52:06 +0800

    chore: migrate video_player to quickjs support.

--- packages/kraken_video_player/lib/kraken_video_player.dart
+++ packages/kraken_video_player/lib/kraken_video_player.dart
@@ -1,23 +1,9 @@
-import 'package:flutter/widgets.dart';
 import 'package:kraken/bridge.dart';
 import 'package:kraken/dom.dart';
-import 'dart:ffi';
 import 'video_element.dart';
-import 'platform.dart';
-
-typedef Native_InitBridge = Void Function();
-typedef Dart_InitBridge = void Function();
-
-final Dart_InitBridge _initBridge =
-nativeDynamicLibrary.lookup<NativeFunction<Native_InitBridge>>('initBridge').asFunction();
-
-void initBridge() {
-  _initBridge();
-}
 
 class KrakenVideoPlayer {
   static void initialize() {
-    initBridge();
     ElementManager.defineElement(
         'VIDEO',
         (id, nativePtr, elementManager) => VideoElement(
