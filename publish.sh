6b696533e1041d5e7fffd5e185be96c1f98386ae
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2022-01-12 20:31:50 +0800

    fix: fix publish scripts

--- publish.sh
+++ publish.sh
@@ -7,19 +7,8 @@ publish() {
   cd $ROOT
 }
 
-build_and_publish() {
-  cd $ROOT/packages/$1
-  flutter pub get
-  rm -f $ROOT/packages/$1/ios/$1.xcframework
-  kraken-npbt clean
-  kraken-npbt configure
-  kraken-npbt build
-  flutter pub publish --force
-  cd $ROOT
-}
-
 publish kraken_animation_player
 publish kraken_camera
 publish kraken_video_player
-build_and_publish kraken_websocket
+publish kraken_websocket
 publish kraken_webview
